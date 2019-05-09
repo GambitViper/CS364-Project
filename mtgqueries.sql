fa6f76ed9ef525e87d90f041a7dd12ea5d5936c2
Zach Baklund <baklund.zachary@uwlax.edu>
2019-05-09 13:40:48 -0500

    deck queries

--- mtgqueries.sql
+++ mtgqueries.sql
@@ -23,28 +23,66 @@ CREATE TABLE Has_Deck(
 -- Add users
 INSERT INTO User (user_id, first_name, last_name, collection_id, username, password) VALUES( 1,'zach','baklund',1,'user','user');
 
--- Add Collections for users
-INSERT INTO Collection(collection_id, collection_size) VALUES(1, 0);
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~IN DECK QUERIES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 -- Add Decks into collections
 INSERT INTO Deck(deck_id, deck_name, deck_wins, deck_losses, creation_date) VALUES(1, 'noob slayer', 3, 5, '05/03/2018');
 
+-- Add card to a collection's deck
+INSERT INTO In_Deck(deck_id, card_id, card_amount) VALUES(1, 435152, 1); --replace val 1 w/ deck_id, val 2 w/ card_id, val 3 w/ card_amount
+
+-- Add one card to In_Deck card count
+UPDATE In_Deck
+SET card_amount = 1 + (SELECT card_amount
+                       FROM In_Deck
+                       WHERE deck_id == 1 AND card_id == 435152) -- replace 2 with new values
+WHERE card_id == 435152 AND deck_id == 1; --replace with card_id and deck_id
+
+-- Delete one card from In_Deck card count
+UPDATE In_Deck
+SET card_amount = (SELECT card_amount
+                   FROM In_Deck
+                   WHERE deck_id == 1 AND card_id == 435152) - 1 -- replace 2 with new values
+WHERE card_id == 435152 AND deck_id == 1; --replace with card_id and deck_id
+
+-- Add one to win count for a Deck
+UPDATE In_Deck
+SET deck_wins = 1 + (SELECT deck_wins
+                     FROM In_Deck
+                     WHERE deck_id == 1) -- replace 1 with new values
+WHERE deck_id == 1; --replace with deck_id
+
+-- Add one to loss count for a Deck
+UPDATE In_Deck
+SET deck_losses = 1 + (SELECT deck_losses
+                       FROM In_Deck
+                       WHERE deck_id == 1) --replace 1 with new values
+WHERE deck_id == 1; --replace with deck_id
+
+-- Change Deck Name
+UPDATE In_Deck
+SET deck_name = 'new name'
+WHERE deck_id == 1; --replace with deck_id
+
+-- ~~~~~~~~~~~~~~~~~~~IN COLLECTION QUERIES~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 -- Add Card to user collection
-INSERT INTO In_Collection(card_id, collection_id, amount) VALUES(435290, 1, 2);
+INSERT INTO In_Collection(card_id, collection_id, amount) VALUES(435152, 1, 1); --replace 3 with new values
 
 --Add one to card count
 UPDATE In_Collection
 SET amount = 1 + (SELECT amount
                   FROM In_Collection 
                   WHERE collection_id == 1 AND card_id == 435289)-- replace 2 with new values
-WHERE card_id = 435289; -- replace with card_id
+WHERE card_id = 435289 AND collection_id == 1; -- replace with card_id abd collection_id
 
 --Delete one card in collection
 UPDATE In_Collection
 SET amount = (SELECT amount
               FROM In_Collection
-              WHERE collection_id == 1 AND card_id == 435289) - 1
-WHERE card_id == 435289;
+              WHERE collection_id == 1 AND card_id == 435289) - 1 --replace 2 with new values
+WHERE card_id == 435289 AND collection_id == 1; --replace with card_id and collection_id
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~ SUMMARY QUERIES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 -- Updating collection size
 UPDATE Collection
@@ -56,9 +94,6 @@ WHERE Collection.collection_id == 1;
 -- Deleting tuples if amount is 0 or less
 DELETE FROM In_Collection
 WHERE amount <= 0;
-
--- Add card to a collection's deck
-INSERT INTO In_Deck(deck_id, card_id, card_amount) VALUES(1, 435152, 1);
 
 -- Total collection price
 SELECT sum(usd_price * amount), sum(usd_foil_price  * amount)
